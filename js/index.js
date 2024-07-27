7f4f1f61fa19256f0c7e9f06e0b2f8ef0df7fd5e
Criel14 <1298373079@qq.com>
2024-07-27 21:06:17 +0800

    在欢迎页添加设备判断

--- js/index.js
+++ js/index.js
@@ -3,3 +3,16 @@ const puzzleGameTitle = document.getElementById('puzzle-game-title');
 window.onload = function () {
     puzzleGameTitle.addEventListener('click', () => window.location.href = 'puzzle.html');
 }
+
+// 判断是否是移动端设备
+function isMobileDevice() {
+    return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
+
+document.getElementById('enterButton').addEventListener('click', function() {
+    if (isMobileDevice()) {
+        window.location.href = 'puzzleMobile.html';
+    } else {
+        window.location.href = 'puzzle.html';
+    }
+});
